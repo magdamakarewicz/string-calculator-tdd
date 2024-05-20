f12282b4da2513ade2a3c79ce549ddb7e1b0ef5c
magdalenamakarewicz <makarewicz.magdalena@outlook.com>
2024-05-20 21:53:11 +0200

    Enable string calculator to handle different delimiters specified in the input string

--- src/main/java/org/example/stringcalculator/model/Parser.java
+++ src/main/java/org/example/stringcalculator/model/Parser.java
@@ -2,10 +2,12 @@ package org.example.stringcalculator.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Parser {
 
     private final String input;
+    private String delimiter = "[,\n]";
     private final List<Integer> resultNumbers = new ArrayList<>();
 
     public Parser(String input) {
@@ -14,7 +16,13 @@ public class Parser {
 
     public List<Integer> parse() {
         checkLastCharacter();
-        processNumbers(input.split(",|\n"));
+
+        if (input.startsWith("//")) {
+            parseWithCustomDelimiter();
+        } else {
+            parseDefault();
+        }
+
         return resultNumbers;
     }
 
@@ -25,6 +33,10 @@ public class Parser {
         }
     }
 
+    private void parseDefault() {
+        processNumbers(input.split(delimiter));
+    }
+
     private void processNumbers(String[] tokens) {
         for (String token : tokens) {
             if (!token.isEmpty()) {
@@ -32,6 +44,42 @@ public class Parser {
                 resultNumbers.add(number);
             }
         }
+    }
+
+    private void parseWithCustomDelimiter() {
+        int delimiterIndex = input.indexOf("\n");
+        delimiter = input.substring(2, delimiterIndex);
+        String[] tokens = input.substring(delimiterIndex + 1).split(Pattern.quote(delimiter));
+        processNumbersWithCustomDelimiter(tokens);
+    }
+
+    private void processNumbersWithCustomDelimiter(String[] tokens) {
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                char nonNumericChar = findNonNumericChar(token);
+                if (nonNumericChar != 0) {
+                    processTokenWithIncorrectDelimiter(nonNumericChar);
+                } else {
+                    int number = Integer.parseInt(token.trim());
+                    resultNumbers.add(number);
+                }
+            }
+        }
+    }
+
+    private char findNonNumericChar(String str) {
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return c;
+            }
+        }
+        return 0;
+    }
+
+    private void processTokenWithIncorrectDelimiter(char nonNumericChar) {
+        int position = input.indexOf(nonNumericChar) - input.indexOf("\n") - 1;
+        throw new IllegalArgumentException("'" + delimiter + "' expected but '" + nonNumericChar +
+                "' found at position " + position + ".");
     }
 
 }
